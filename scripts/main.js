caf3a57cea806c207fa4806eb089fd5b4f2d34cd
patrick <patrick.mcisaac.dev@gmail.com>
2025-07-25 16:02:37 -0500

    fetched

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 const foxButton = document.querySelector("#fox-button")
 foxButton.addEventListener("click", () => {
-  
+    fetch('https://randomfox.ca/floof/')
 })
 
 const dogButton = document.querySelector("#dog-button")
 dogButton.addEventListener("click", () => {
-  
+    fetch('https://random.dog/woof.json')
 })
+
+// NEXT IS THEN WE DISPLAY
