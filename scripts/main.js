444bdd2c8fd9472f09bca8000b11ce71146730d9
patrick <patrick.mcisaac.dev@gmail.com>
2025-07-25 15:50:28 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const foxButton = document.querySelector("#fox-button")
+foxButton.addEventListener("click", () => {
+  
+})
+
+const dogButton = document.querySelector("#dog-button")
+dogButton.addEventListener("click", () => {
+  
+})
