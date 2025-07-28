eecc3f9244689cf31e2171d225c290055673bcb4
patrick <patrick.mcisaac.dev@gmail.com>
2025-07-28 09:22:59 -0500

    then()

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,22 @@
 const foxButton = document.querySelector("#fox-button")
 foxButton.addEventListener("click", () => {
-    fetch('https://randomfox.ca/floof/')
+    fetch('https://randomfox.ca/floof/').then((response) => {
+        // this code runs when the Promise is fulfilled
+        console.log('Response received!')
+        return response.json()
+    }).then((foxData) => {
+        const foxImage = document.querySelector("#fox")
+        foxImage.src = foxData.image
+    })
+    console.log('This code runs immediately!');
 })
 
 const dogButton = document.querySelector("#dog-button")
 dogButton.addEventListener("click", () => {
-    fetch('https://random.dog/woof.json')
+    fetch('https://random.dog/woof.json').then((response) => {
+        return response.json()
+    }).then((dogData) => {
+        const dogImage = document.querySelector('#dog')
+        dogImage.src = dogData.url
+    })
 })
-
-// NEXT IS THEN WE DISPLAY
